91a64216645712d5bd69c651ab4399a6ccece6d0
Zacchaeus-Oluwole <zacchaeusoluwole@gmail.com>
2022-06-28 08:52:04 +0100

    adding rest api

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+from .views import PostViewSet, UserViewSet
+from rest_framework.routers import SimpleRouter
+
+router = SimpleRouter()
+
+router.register('users', UserViewSet, basename='users')
+router.register('',PostViewSet, basename='post')
+
+urlpatterns = router.urls
--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from .models import Post
+from .serializers import PostSerializer, UserSerializer
 
 # Create your views here.
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
