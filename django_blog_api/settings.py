3b2c69487e8f5867780fc82d36740f9163bdb20c
Zacchaeus-Oluwole <zacchaeusoluwole@gmail.com>
2022-06-29 21:45:22 +0100

    Adding Authentication

--- django_blog_api/settings.py
+++ django_blog_api/settings.py
@@ -38,15 +38,25 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #3rd party apps
     'api',
     'rest_framework',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'allauth', # new
+    'allauth.account', # new
+    'allauth.socialaccount', # new
 ]
 
 # new
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [ # new
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
+    ],
 }
 
 MIDDLEWARE = [
