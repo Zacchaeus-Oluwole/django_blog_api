2ef6e1cba9da8af1c06bb0c298c78cc1d1ddc80a
Zacchaeus-Oluwole <zacchaeusoluwole@gmail.com>
2022-06-29 13:51:09 +0100

    ...

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Post
 from .serializers import PostSerializer, UserSerializer
 from .permission import IsAuthorOrReadOnly
@@ -9,7 +9,7 @@ from .permission import IsAuthorOrReadOnly
 
 
 class PostViewSet(viewsets.ModelViewSet):
-    permission_classes = (IsAuthorOrReadOnly,)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsAuthorOrReadOnly,]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
